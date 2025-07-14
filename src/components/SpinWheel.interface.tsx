928883466b3304fb50f3131304d162f2ac25fcf3
sks-poc <skslearning.jpr@gmail.com>
2025-07-14 18:31:02 +0530

    Add files via upload

---
+++ src/components/SpinWheel.interface.tsx
@@ -0,0 +1,20 @@
+export interface ISpinWheelProps {
+  segments: ISegments[];
+  onFinished: (result: string) => void;
+  primaryColor?: string;
+  contrastColor?: string;
+  buttonText?: string;
+  isOnlyOnce?: boolean;
+  size?: number;
+  upDuration?: number;
+  downDuration?: number;
+  fontFamily?: string;
+  arrowLocation?: 'center' | 'top';
+  showTextOnSpin?: boolean;
+  isSpinSound?: boolean;
+}
+
+export interface ISegments {
+  segmentText: string;
+  segColor?: string;
+}
