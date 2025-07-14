75955e5f6389d5ce214e6a2b431d3c242b84cf5b
sks-poc <skslearning.jpr@gmail.com>
2025-07-14 18:23:08 +0530

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { ISegments, ISpinWheelProps } from "./components/SpinWheel.interface"
+import SpinWheel from "./components/SpinWheel"
+
+export type { ISegments, ISpinWheelProps }
+export { SpinWheel }
